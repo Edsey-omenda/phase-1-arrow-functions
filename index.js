731c30bbaf8dfd3a866dc8bfddbc8b98237c0e2e
Edsey omenda <omendajames0317@gmail.com>
2022-06-08 15:50:27 +0300

    commit tree 4fa716166431574ae989524119e288c0d858e4dd

---
+++ index.js
@@ -0,0 +1,19 @@
+let num1 = 2000;
+let num2 = 100;
+const divide = () => num1 / num2;{
+    console.log(num1 / num2);
+}
+divide ();
+
+let num3 = 2;
+const square = () => num3 * num3;{
+     console.log(num3 * num3);
+}
+square();
+
+let num7 = 2;
+let num8 = 5;
+const add = () => num7 + num8;{
+    console.log(num7 + num8);
+}
+add();
